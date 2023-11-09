39b11552e5a7e22867b4a05bd8ae1cb9581e5cf5
Sohan916 <sohan96973552@gmail.com>
2023-11-09 09:36:02 +0530

    initial commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+final randomize = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-2.png';
+  var diceRoll = 2;
+
+  void rollDice() {
+    setState(() {
+      diceRoll = randomize.nextInt(6) + 1;
+      activeDiceImage = 'assets/images/dice-$diceRoll.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset('assets/images/dice-$diceRoll.png', width: 200),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
+  }
+}
